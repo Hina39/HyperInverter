477185a2c9bfb1006fd28dc1803574a5e6678acc
gatheluck <gatheluck+tech@gmail.com>
2023-11-26 17:55:44 +0900

    fix: quick fix for generate_from_latent.py

--- scripts/generate_from_latent.py
+++ scripts/generate_from_latent.py
@@ -11,12 +11,9 @@ sys.path.append("..")
 
 import numpy as np  # noqa: E402
 import torch  # noqa: E402
-from configs import data_configs  # noqa: E402
-from datasets.inference_dataset import InferenceDataset  # noqa: E402
 from models.hyper_inverter import HyperInverter  # noqa: E402
 from options.test_options import TestOptions  # noqa: E402
 from PIL import Image  # noqa: E402
-from torch.utils.data import DataLoader  # noqa: E402
 from tqdm import tqdm  # noqa: E402
 from utils.common import log_input_image, tensor2im, unflatten_dict  # noqa: E402
 from utils import common
@@ -47,21 +44,21 @@ def run():
     net.cuda()
 
     # データセットとデータローダーを設定
-    dataset_args = data_configs.DATASETS[opts.dataset_type]
-    transforms_dict = dataset_args["transforms"](opts).get_transforms()
-    dataset = InferenceDataset(
-        root=opts.data_path, transform=transforms_dict["transform_inference"], opts=opts
-    )
-    dataloader = DataLoader(
-        dataset,
-        batch_size=opts.batch_size,
-        shuffle=False,
-        num_workers=int(opts.workers),
-        drop_last=False,
-    )
+    # dataset_args = data_configs.DATASETS[opts.dataset_type]
+    # transforms_dict = dataset_args["transforms"](opts).get_transforms()
+    # dataset = InferenceDataset(
+    #     root=opts.data_path, transform=transforms_dict["transform_inference"], opts=opts
+    # )
+    # dataloader = DataLoader(
+    #     dataset,
+    #     batch_size=opts.batch_size,
+    #     shuffle=False,
+    #     num_workers=int(opts.workers),
+    #     drop_last=False,
+    # )
 
-    if opts.n_images is None:
-        opts.n_images = len(dataset)
+    # if opts.n_images is None:
+    #     opts.n_images = len(dataset)
 
     # データローダーからバッチを取得し、それぞれのバッチに対して以下の操作を行います：
     # バッチをGPUに移動し、float型に変換します。
@@ -118,9 +115,20 @@ def run():
     # predicted_weights, w_codes = np.load("/home/challenger/hyperinverter/outputs2/latents/10380049_598442736930128_1821329686642729572_o.npz")
 
 
-def run_on_batch(npz_file, net):
-    predicted_weights = npz_file["predicted_weights"]
-    w_codes = npz_file["w_codes"]
+def run_on_batch(latents, net):
+    """
+
+    Args:
+        latents (Dict):
+            "predicted_weights"と"w_codes"をkeyとして持つ辞書.
+            save_latent.pyによって保存される. 含まれる重みは既にtorch.Tensorに
+            変換されていることを想定.
+
+        net (nn.Module):
+
+    """
+    predicted_weights = latents["predicted_weights"]
+    w_codes = latents["w_codes"]
 
     predicted_weights = {
         key: torch.from_numpy(value).to("cuda")
@@ -129,9 +137,9 @@ def run_on_batch(npz_file, net):
     w_codes = torch.from_numpy(w_codes).to("cuda")
 
     with torch.no_grad():
-            w_images = net.decoder[0].synthesis(
-                w_codes, added_weights=None, noise_mode="const"
-            )
+        w_images = net.decoder[0].synthesis(
+            w_codes, added_weights=None, noise_mode="const"
+        )
 
     # forwardもよばれる　net()はHyperInverterクラスの__call__メソッドを呼び出している
 
@@ -174,4 +182,71 @@ def run_on_batch(npz_file, net):
 
 
 if __name__ == "__main__":
-    run()
+    # run()
+
+    import pathlib
+
+    # 一旦ここでテストする
+    test_opts = TestOptions().parse()
+    out_path_results = os.path.join(test_opts.exp_dir, "inference_results")
+    out_path_coupled = os.path.join(test_opts.exp_dir, "inference_coupled")
+
+    os.makedirs(out_path_results, exist_ok=True)
+    os.makedirs(out_path_coupled, exist_ok=True)
+
+    ckpt = torch.load(test_opts.checkpoint_path, map_location="cpu")
+    opts = ckpt["opts"]
+    opts.update(vars(test_opts))
+    opts = argparse.Namespace(**opts)
+    net = HyperInverter(opts)
+    net.eval()
+    net.cuda()
+
+    npz_path = pathlib.Path("outputs/latents/63000.npz")
+    latents = unflatten_dict(np.load(npz_path), separator="-")
+    predicted_weights = latents["predicted_weights"]
+    w_codes = latents["w_codes"]
+
+    predicted_weights = {
+        key: torch.from_numpy(value).to("cuda")
+        for key, value in predicted_weights.items()
+    }
+    w_codes = torch.from_numpy(w_codes).to("cuda")
+
+    with torch.no_grad():
+        w_images = net.decoder[0].synthesis(
+            w_codes, added_weights=None, noise_mode="const"
+        )
+
+    print(w_images.shape)  # torch.Size([1, 3, 1024, 1024])
+
+    pred_weights_per_sample = {}
+    for key in predicted_weights:
+        pred_weights_per_sample[key] = predicted_weights[key][0]
+
+    added_weights = common.convert_predicted_weights_to_dict(pred_weights_per_sample)
+
+    final_image = (
+        net.decoder[0]
+        .synthesis(w_codes, added_weights=added_weights, noise_mode="const")
+        .squeeze(0)
+    )
+
+    print(final_image.shape)
+
+    # 　画像を保存する.
+    _w_image = tensor2im(w_images[0])
+    _final_image = tensor2im(final_image)
+
+    res = np.concatenate(
+        [np.array(_w_image), np.array(_final_image)],
+        axis=1,
+    )
+    Image.fromarray(res).save(
+        os.path.join(out_path_coupled, os.path.basename(npz_path.stem) + ".png")
+    )
+
+    im_save_path = os.path.join(
+        out_path_results, os.path.basename(npz_path.stem) + ".png"
+    )
+    Image.fromarray(np.array(_final_image)).save(im_save_path)
